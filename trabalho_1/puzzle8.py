c775e1362b56e96640aa2c53fc49bdf3dfac909c
Guilherme Barbosa <guilhermecirineu@hotmail.com>
2026-03-29 12:58:26 -0300

    Add puzzle8 algorithm

---
+++ trabalho_1/puzzle8.py
@@ -0,0 +1,179 @@
+import sys
+import time
+
+expected_output = ['1', '2', '3', '4', '5', '6', '7', '8', '0']
+sys.setrecursionlimit(90000)
+class Node:
+    def __init__(self, father = None, current_state = None, visited = False, direction = None):
+        self.father = father
+        self.current_state = current_state
+        self.visited = visited
+        self.direction = direction
+        
+    def get_index(self):  
+        return self.current_state.index("0")
+    
+    def catch_neiboughrs(self):
+        if self.catch_neiboughrs == None:
+            return []
+        index = self.current_state.index("0")
+
+        if index == 0:
+            return [1, 3]
+        elif index == 1:
+            return [0, 2, 4]
+        elif index == 2:
+            return [1, 5]
+        elif index == 3:
+            return [0, 4, 6]
+        elif index == 4:
+            return [1, 3, 5, 7]
+        elif index == 5:
+            return [2, 4, 8]
+        elif index == 6:
+            return [3, 7] 
+        elif index == 7:
+            return [4, 6, 8]
+        elif index == 8:
+            return [5, 7]  
+
+    def show_pretty_matrix(self):
+        count = 0
+        for _ in range(3):
+            arquivo.write("|")             
+            for j in range(3):
+                if j == 2:
+                    arquivo.write(f"{self.current_state[j+count]}") 
+                else:
+                    arquivo.write(f"{self.current_state[j+count]} ")                    
+            arquivo.write("|\n")     
+            count += 3
+
+    def get_direction(self, current, next, number):
+        if current > next:
+            if current - next == 1:
+                self.direction = f"{number} para esquerda"
+            else:
+                self.direction = f"{number} para cima"
+        else:
+            if next - current == 1:
+                self.direction = f"{number} para direita"
+            else:
+                self.direction = f"{number} para baixo"               
+
+def dfs_recursive(node):
+
+    state_tuple = tuple(node.current_state)
+    
+    if state_tuple not in visited_states:
+        visited_states.add(state_tuple)
+    else:
+        return None
+
+    if node.current_state == expected_output:
+        arquivo.write("Sucesso.")
+        return node
+    
+    index = node.get_index()
+    len_neighbours = len(node.catch_neiboughrs())
+    neighbours = node.catch_neiboughrs()
+ 
+    for i in range(len_neighbours):   
+       
+        new_state = node.current_state.copy()
+        new_state[neighbours[i]], new_state[index] = new_state[index], new_state[neighbours[i]]
+
+        result = dfs_recursive(Node(node,new_state, True))  
+        if result is not None:
+            return result 
+    return None
+        
+
+def has_solution(list):
+    count = 0
+    for i in range(len(list)-1):
+        if list[i] == "0":
+            continue
+        for j in range(i+1,len(list)):
+            if list[j] == "0":
+                continue
+            if list[i] > list[j]:
+                count += 1  
+    return [count%2 == 0, count]            
+
+algorithm = sys.argv[1]
+
+if algorithm == "BFS":
+    file_name = 'bfs.txt'
+else:
+    file_name = 'dfs.txt'    
+
+with open(file_name, 'w', encoding='utf-8') as arquivo:
+    inicio = time.time()
+
+    current_state = list(sys.argv[2:])
+
+    visited_states = set()
+    nodes_list = []
+    initial_node = Node(None, current_state, True)
+    nodes_list.append(initial_node)
+
+    has_solution, count = has_solution(current_state)
+
+    arquivo.write(f"Tem solução? {has_solution}, paridade? {count}\n")
+
+    if(has_solution is False):
+        exit()
+
+
+    visited_states.add(tuple(current_state))
+    count_visits = 0
+    while(len(nodes_list) != 0):
+        if algorithm == "BFS":
+            node = nodes_list.pop(0)  
+        else:
+            node = nodes_list.pop()    
+
+        if node.current_state == expected_output:
+            arquivo.write("Sucesso.\n")
+            break
+        
+        index = node.get_index()
+        len_neighbours = len(node.catch_neiboughrs())
+        neighbours = node.catch_neiboughrs()
+
+        for i in range(len_neighbours):   
+
+            new_state = node.current_state.copy()
+            new_state[neighbours[i]], new_state[index] = new_state[index], new_state[neighbours[i]]
+
+
+            state_tuple = tuple(new_state)
+
+            if state_tuple not in visited_states:
+                count_visits += 1
+                visited_states.add(state_tuple)
+                newNode = Node(node,new_state, True)
+                nodes_list.append(newNode)
+
+                newNode.get_direction(neighbours[i], index, new_state[index])
+               
+        
+    count = 0        
+    while(node):
+        
+        node.show_pretty_matrix()
+        arquivo.write("\n")
+        arquivo.write(f"Sentido: {node.direction}\n")   
+        node = node.father
+        count+=1
+    arquivo.write(f"Número de ancestrais: {count - 1}\n")  
+    arquivo.write(f"Visitas totais: {count_visits}\n")
+
+    fim = time.time()
+    arquivo.write(f"Tempo total de execução: {fim - inicio} segundos")
+
+
+
+
+
---
+++ trabalho_1/relatorio.md
@@ -0,0 +1,31 @@
+
+## Relatório - Resolução do 8-Puzzle por Busca em Largura e Busca em Profundidade
+
+### Como executar:
+```bash
+python3 puzzle8.py BFS 1 2 3 0 4 5 6 8 7
+```
+
+- Considera-se o `0` o espaço vazio do jogo.
+
+- Será gerado um arquivo `dfs.txt` ou `bfs.txt` a depender do algortimo escolhido. Nele vão haver informações como o tempo de execução, o número de nós visitados, o número de ancestrais e a exibição deles (de baixo para cima) até chegar na resolução e o sentido escolhido.
+--- 
+
+### Modelagem:
+
+1.  O estado inicial do problema será a entrada inserida no comando, como exemplificada acima;
+2. O estado objetivo será quando a sequência de números estiver ordenada em 1 2 3 4 5 6 7 8 0;
+3. O conjunto de ações conterá os sentidos ou vizinhança que um nó pode ter. Considera-se então, dado cada situação específica de um índice i, se ele pode seguir para as posições `i + 1`, `i - 1` , `i + 3` ou `i - 3`; 
+4. A função sucessora indicará o próximo estado a ser alcançando, que ainda não tenha sido expandido;
+5. O teste de objetivo verificará se o estado objetivo foi atingido, a cada novo nó visitado;
+6. A representação do nó de busca será a classe **`Ǹode`**, que conterá os atributos `father`, `current_state`, `visited`, `direction`
+7. O algoritmo utiliza a estrutra de dados *`Set`* para controlar que cada nó com um dado estado só pode ser armazenado uma vez, e impedir uma complexidade exponencial.
+
+### Análise comparitiva
+
+- Observou-se a profundidade do estado objetivo com o BFS é quase sempre menor do que a produndidade obtida pelo DFS, o que é natural, já que é da propriedade do algoritmo DFS encontrar sempre explorar ao máximo a profundidade de um nó. Porém, tanto o consumo de memória quanto o tempo de execução variam muito já que irá depender da posição mais relevante na busca do estado objetivo na árvore, se ela se encontra nos extremos ou mais centralizada.
+
+- Como os pesos das arestas não são relevantes para o problema, a questão de otimalidade não é levada em conta, apenas a estrutura de dado (Pilha ou Fila) escolhida é considerada como distinção principal.
+ 
+O algoritmo executa em tempo `O(V + E)` com memoização aplicada, considerando V os vértices (nós), e E as arestas (vizinhos) de cada vértice.
+
